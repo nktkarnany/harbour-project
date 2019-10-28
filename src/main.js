a2ed14642605cfcc4d2c52fffe978f673d5e18b4
Ankit Karnany <nktkarnany@Ankits-MacBook-Pro.local>
2019-10-28 20:07:18 +0100

    color transition added

--- src/main.js
+++ src/main.js
@@ -5,22 +5,13 @@ import { TweenMax, TimelineLite, Power2, Elastic, CSSPlugin } from "gsap/TweenMa
 
 $(function () {
 
-    $(".aside__container a").hover(function (e) {
-        TweenMax.to(e.target, 0.3, {
-            color: '#000000'
-        });
-        TweenMax.to(e.target.firstElementChild, 0.3, {
-            width: '10px'
-        });
-    }, function (e) {
-        TweenMax.to(e.target, 0.3, {
-            color: '#000'
-        });
-        TweenMax.to(e.target.firstElementChild, 0.3, {
-            width: '0'
-        });
-    });
+    let COLOR = {
+        yellow: "rgb(255, 193, 7)",
+        red: "rgb(239, 83, 80)",
+        blue: "rgb(33, 150, 243)"
+    };
 
+    // Toggle Bar Animation
     let toggleAnimation = {
         whole: TweenMax.to($('.aside-toggle'), 1, {
             left: '-56px',
@@ -57,6 +48,34 @@ $(function () {
             toggleAnimation.middleBar.reverse();
             toggleAnimation.bottomBar.reverse();
         }
+
+    });
+
+    // Hover Animation of Side Links in Menu
+    $(".aside__container a").hover(function (e) {
+        TweenMax.to(e.target.firstElementChild, 0.3, {
+            width: '10px'
+        });
+    }, function (e) {
+        TweenMax.to(e.target.firstElementChild, 0.3, {
+            width: '0'
+        });
+    });
+
+    // Click Animation of Side Links in Menu
+    $(".aside__container a").click(function (e) {
+        
+        $(this).parent().find('a').removeClass();
+
+        let dataColor = $(this).data("color");
+
+        let colorClass = "active--" + dataColor;
+
+        $(this).addClass("active " + colorClass);
+
+        TweenMax.to(".main-section", 0.5, {
+            backgroundColor: COLOR[dataColor]
+        });
 
     });
 
