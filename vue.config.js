f8df9cee5f30c7d0d5831fd1e8b47790039b8eb4
Ankit Karnany <nktkarnany@Ankits-MacBook-Pro.local>
2020-04-05 19:16:55 +0200

    added contact form in about page

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,39 @@ module.exports = {
         use: [rupture()],
       }
     }
+  },
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Ankit Karnany',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    about: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/about.html',
+      // output as dist/index.html
+      filename: 'about.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'About Me',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
   }
 };
